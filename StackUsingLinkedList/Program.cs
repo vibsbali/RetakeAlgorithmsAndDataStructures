1ef79b1234a6dcca3b8be9ec261d0580adfc611a
Vaibhav Bali <vibsbali@gmail.com>
2017-07-13 21:18:32 +1000

    Implemented Stack using LinkedList

---
+++ StackUsingLinkedList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace StackUsingLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<string>();
+
+            stack.Push("One");
+            stack.Push("Two");
+            stack.Push("Three");
+
+            foreach (var value in stack)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.WriteLine($"Total count = {stack.Count}");
+
+            while (stack.Count != 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+
+            Console.WriteLine(stack.Peek());
+
+            stack.Push("One");
+            Console.WriteLine(stack.Peek());
+        }
+    }
+}
---
+++ StackUsingLinkedList/Stack.cs
@@ -0,0 +1,68 @@
+﻿namespace StackUsingLinkedList
+{
+    public class Stack<T> : System.Collections.Generic.IEnumerable<T>
+    {
+        private readonly System.Collections.Generic.LinkedList<T> BackingStore =
+            new System.Collections.Generic.LinkedList<T>();
+
+        public System.Collections.Generic.IEnumerator<T> GetEnumerator()
+        {
+            return BackingStore.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Push(T item)
+        {
+            BackingStore.AddFirst(item);
+        }
+
+        public void Clear()
+        {
+            BackingStore.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return BackingStore.Find(item) != null;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            var current = BackingStore.First;
+            while (current != null)
+            {
+                array[arrayIndex++] = current.Value;
+                current = current.Next;
+            }
+        }
+
+        public T Peek()
+        {
+            if (Count != 0)
+            {
+                return BackingStore.First.Value;
+            }
+
+            throw new System.InvalidOperationException("Stack empty");
+        }
+
+        public T Pop()
+        {
+            if (Count != 0)
+            {
+                var head = BackingStore.First;
+                BackingStore.RemoveFirst();
+                return head.Value;
+            }
+
+            throw new System.InvalidOperationException("Stack empty");
+        }
+
+        public int Count => BackingStore.Count;
+        public bool IsReadOnly => false;
+    }
+}
