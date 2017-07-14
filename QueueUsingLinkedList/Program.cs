f1cae2a3fc8724c655faffe45787bfc80507e842
Bali Vaibhav <bail.vaibhav@recreo.com.au>
2017-07-14 14:24:14 +1000

    Created Queue shell

---
+++ QueueUsingLinkedList/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace QueueUsingLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ QueueUsingLinkedList/Queue.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace QueueUsingLinkedList
+{
+    public class Queue<T> : System.Collections.Generic.IEnumerable<T>
+    {
+        private readonly System.Collections.Generic.LinkedList<T> backingStore = new System.Collections.Generic.LinkedList<T>();
+
+        public void Enqueue(T value)
+        {
+            backingStore.AddLast(value);
+        }
+
+        public T Dequeue()
+        {
+            if (backingStore.Count == 0)
+            {
+                throw new System.InvalidOperationException("Empty queue");
+            }
+
+            var value = backingStore.First.Value;
+            backingStore.RemoveFirst();
+            return value;
+        }
+
+        public T Peek()
+        {
+            if (backingStore.Count == 0)
+            {
+                throw new System.InvalidOperationException("Empty queue");
+            }
+
+            return backingStore.First.Value;
+        }
+
+        public int Cout => backingStore.Count;
+        public bool IsReadOnly => false;
+        public System.Collections.Generic.IEnumerator<T> GetEnumerator()
+        {
+            return backingStore.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
