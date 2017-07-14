bb228cdd30740b98fa6a9331857557f3ab5650c3
Vaibhav Bali <vibsbali@gmail.com>
2017-07-14 21:39:22 +1000

    Implemented Queue using array

---
+++ QueueUsingArrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueUsingArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue<string>();
+
+            queue.Enqueue("One");
+            queue.Enqueue("Two");
+
+            Console.WriteLine(queue.Deque());
+            
+            queue.Enqueue("Three");
+            queue.Enqueue("Four");
+
+            Console.WriteLine(queue.Deque());
+
+            queue.Enqueue("Five");
+            queue.Enqueue("Six");
+            queue.Enqueue("Seven");
+            queue.Enqueue("Eight");
+            queue.Enqueue("Nine");
+            queue.Enqueue("Ten");
+
+            foreach (var value in queue)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
---
+++ QueueUsingArrays/Queue.cs
@@ -0,0 +1,109 @@
+﻿namespace QueueUsingArrays
+{
+    public class Queue<T> : System.Collections.Generic.IEnumerable<T>
+    {
+        private int head = 0;
+        private int tail = -1;
+        private T[] backingArray;
+
+        public int Count { get; private set; }
+
+        public Queue(int size)
+        {
+            backingArray = new T[size];
+        }
+
+        public Queue()
+            : this(4)
+        {
+        }
+
+        public void Enqueue(T item)
+        {
+            if (Count == backingArray.Length)
+            {
+                SizeUp();
+            }
+            else if (tail == backingArray.Length - 1)
+            {
+                tail = -1;
+            }
+            
+
+            backingArray[++tail] = item;
+            Count++;
+        }
+
+        public T Deque()
+        {
+            if (Count == 0)
+            {
+                throw new System.InvalidOperationException("Empty queue");
+            }
+
+            var item = backingArray[head];
+            backingArray[head] = default(T);
+            //If head is at the last index
+            if (head == backingArray.Length - 1)
+            {
+                head = 0;
+            }
+            else
+            {
+                head++;
+            }
+
+            --Count;
+            return item;
+        }
+
+        private void SizeUp()
+        {
+            var tempArray = new T[Count * 2];
+
+            //check if we have wrapped
+            if (head > tail)
+            {
+                System.Array.Copy(backingArray, head, tempArray, 0, backingArray.Length - head);
+                System.Array.Copy(backingArray, 0, tempArray, backingArray.Length - head, tail + 1);
+            }
+            else
+            {
+                System.Array.Copy(backingArray, 0, tempArray, 0, Count);
+            }
+
+            backingArray = tempArray;
+            head = 0;
+            tail = Count - 1;
+        }
+
+        public System.Collections.Generic.IEnumerator<T> GetEnumerator()
+        {
+            //are we wrapped
+            if (head > tail)
+            {
+                for (int i = head; i < backingArray.Length; i++)
+                {
+                    yield return backingArray[i];
+                }
+
+                for (int i = 0; i <= tail; i++)
+                {
+                    yield return backingArray[i];
+                }
+            }
+            else
+            {
+                for (int i = head; i <= tail; i++)
+                {
+                    yield return backingArray[i];
+                }
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
